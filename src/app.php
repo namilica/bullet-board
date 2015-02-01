f5fd29fcb81ab6e1aa68bbafe045a20bb03ca300
namilica <kuroain@gmail.com>
2015-02-01 22:38:03 +0800

    using dummy data for prototyping layout

---
+++ src/app.php
@@ -0,0 +1,11 @@
+<?php
+
+use \view\view;
+$template = new view(__DIR__.'/templates', __DIR__.'/storage/cache');
+$posts = [
+	['id' => 0, 'name' => '@non-san', 'post' => 'ur a fa&get'],
+	['id' => 1, 'name' => '@non-kun', 'post' => 'try me'],
+	['id'=> 2, 'name' => '<b>SUpA H@ca', 'post' => 'XSS LOL<script src="ur a fagit">']
+	];
+$data = ['posts' => $posts];
+$template->draw('thread', $data);
